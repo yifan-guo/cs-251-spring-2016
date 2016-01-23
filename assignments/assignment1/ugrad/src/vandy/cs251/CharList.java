6a4cb813c95fd2c727d7d657d95bc5e2ed928e24
Yifan Guo <yifan.guo@vanderbilt.edu>
2016-01-22 18:52:40 -0800

    added private helper method sizeCheck(), resize() prunes nodes

--- assignments/assignment1/ugrad/src/vandy/cs251/CharList.java
+++ assignments/assignment1/ugrad/src/vandy/cs251/CharList.java
@@ -1,14 +1,14 @@
 package vandy.cs251;
 
-import java.lang.IndexOutOfBoundsException;
+        import java.lang.IndexOutOfBoundsException;
 
 /**
  * Provides a wrapper facade around primitive char lists, allowing
  * for dynamic resizing.
  */
-public class CharList 
-             implements Comparable<CharList>, 
-                        Cloneable {
+public class CharList
+        implements Comparable<CharList>,
+        Cloneable {
     /**
      * The head of the list.
      */
@@ -27,7 +27,7 @@ public class CharList
     private char myValue;
 
     /**
-     * Constructs an list of the given size.    (Good)
+     * Constructs an list of the given size.
      *
      * @param size Non-negative integer size of the desired list.
      * @throw IndexOutOfBoundsException If size < 0.
@@ -38,7 +38,7 @@ public class CharList
     }
 
     /**
-     * Constructs an list of the given size, filled with the provided   (Good)
+     * Constructs an list of the given size, filled with the provided
      * default value.
      *
      * @param size Nonnegative integer size of the desired list.
@@ -47,14 +47,11 @@ public class CharList
      */
     public CharList(int size, char defaultValue) {
         // TODO - you fill in here
-        if (size < 0) {
-            throw new IndexOutOfBoundsException("Size cannot be negative!");
-        }
+        sizeCheck(size);
         mySize = size;
         myValue = defaultValue;
-        Node tmp;
         for (int i = 0; i < size; i++) {
-            tmp = new Node();
+            Node tmp = new Node();
             tmp.myChar = defaultValue;
             tmp.next = myHead;
             myHead = tmp;
@@ -62,7 +59,7 @@ public class CharList
     }
 
     /**
-     * Copy constructor; creates a deep copy of the provided CharList.  (Good)
+     * Copy constructor; creates a deep copy of the provided CharList.
      *
      * @param s The CharList to be copied.
      */
@@ -93,20 +90,21 @@ public class CharList
     public Object clone() {
         // TODO - you fill in here (replace return null with right
         // implementation).
-    	return new CharList(this);
+        return new CharList(this);
     }
 
     /**
-     * @return The current size of the list.    (Good)
+     * @return The current size of the list.
      */
     public int size() {
         // TODO - you fill in here (replace return 0 with right
         // implementation).
-    	return mySize;
+        return mySize;
     }
 
     /**
-     * Resizes the list to the requested size.      (Good)
+     * Resizes the list to the requested size.
+     * @throw IndexOutOfBoundsException If size < 0.    (I ADDED THIS LINE!)
      *
      * Changes the capacity of this list to hold the requested number of elements.
      * Note the following optimizations/implementation details:
@@ -120,27 +118,38 @@ public class CharList
      */
     public void resize(int size) {
         // TODO - you fill in here
+        sizeCheck(size);
         if (size > mySize) {
             //initialize myHead
             if (mySize == 0) {
                 myHead = new Node();
                 myHead.myChar = myValue;
+                mySize++;
             }
             //initialize rest of list w/ default value
-            Node prev = myHead;
-            for (int i = 1; i < mySize; i++) {
-                prev = prev.next;
-            }
+            Node prev = seek(mySize-1); //get the last Node in the list
             for (int i = mySize; i < size; i++) {
                 new Node(myValue, prev);
                 prev = prev.next;
             }
         }
+        else if (size < mySize) {
+            if (size == 0) {
+                myHead.prune();
+                myHead = null;
+            }
+            else {
+                seek(size - 1).prune();
+            }
+        }
+        else {  //size == mySize
+            return;
+        }
         mySize = size;
     }
 
     /**
-     * @return the element at the requested index.  (Good)
+     * @return the element at the requested index.
      * @param index Nonnegative index of the requested element.
      * @throws IndexOutOfBoundsException If the requested index is outside the
      * current bounds of the list.
@@ -152,7 +161,7 @@ public class CharList
     }
 
     /**
-     * Sets the element at the requested index with a provided value.   (Good)
+     * Sets the element at the requested index with a provided value.
      * @param index Nonnegative index of the requested element.
      * @param value A provided value.
      * @throws IndexOutOfBoundsException If the requested index is outside the
@@ -164,7 +173,7 @@ public class CharList
     }
 
     /**
-     * Locate and return the @a Node at the @a index location.  (Good)
+     * Locate and return the @a Node at the @a index location.
      */
     private Node seek(int index) {
         // TODO - you fill in here
@@ -190,7 +199,7 @@ public class CharList
     public int compareTo(CharList s) {
         // TODO - you fill in here (replace return 0 with right
         // implementation).
-	    for (int i = 0; i < Math.min(mySize, s.mySize); i++) {
+        for (int i = 0; i < Math.min(mySize, s.mySize); i++) {
             int value = get(i) - s.get(i);
             if (value != 0) {
                 return value;
@@ -209,6 +218,17 @@ public class CharList
         }
     }
 
+    //private helper method
+    /**
+     * Throws an exception if the size parameter is negative.
+     */
+    private void sizeCheck(int size) {
+        // TODO - you fill here
+        if (size < 0) {
+            throw new IndexOutOfBoundsException("Size cannot be negative!");
+        }
+    }
+
     /**
      * A Node in the Linked List.
      */
@@ -216,12 +236,12 @@ public class CharList
         /**
          * Value stored in the Node.
          */
-	// TODO - you fill in here
+        // TODO - you fill in here
         private char myChar;
         /**
          * Reference to the next node in the list.
          */
-	// TODO - you fill in here
+        // TODO - you fill in here
         private Node next;
         /**
          * Default constructor (no op).
@@ -269,3 +289,6 @@ public class CharList
         }
     }
 }
+
+
+
